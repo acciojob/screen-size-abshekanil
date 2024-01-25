f70a0041f786bc003a2aef65f845c41935e121ef
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-25 16:14:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+let h1 = document.getElementById('heading');
+
+function updateSize(){
+	let width = window.innerWidth;
+let height = window.innerHeight;
+	h1.innerHTML = 'Width: '+width+' and Height: '+height;
+}
+
+window.addEventListener('resize', updateSize);
+
+updateSize();
