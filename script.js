39667264e16d04dc487adc99eb0d637779631f47
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-25 16:16:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let h1 = document.getElementById('heading');
 
 function updateSize(){
 	let width = window.innerWidth;
-let height = window.innerHeight;
+	let height = window.innerHeight;
 	h1.innerHTML = 'Width: '+width+' and Height: '+height;
 }
 
